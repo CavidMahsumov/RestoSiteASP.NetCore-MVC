6a7d351798987f2c54e30c576ee7b992debbde2d
Cavid <mehsumovcavid@gmail.com>
2022-04-15 18:02:01 +0400

    Complated

--- RestoSiteASP.NetCore MVC/Data/ImageDbContext.cs
+++ RestoSiteASP.NetCore MVC/Data/ImageDbContext.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RestoSiteASP.NetCore_MVC.Data
+namespace RestoSiteASP.NetCore_MVC.Data 
 {
     public class ImageDbContext:DbContext
     {
